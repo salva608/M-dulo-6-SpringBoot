793bd8afa25a89f6cade997f9c650e7dc4cc4257
salva608 <totoalahielmejor@gmail.com>
2026-05-07 21:01:30 -0500

    feat(repository): implement in-memory EventRepository with Stream API

--- Eventify/src/main/java/com/riwi/eventify/repository/EventRepository.java
+++ Eventify/src/main/java/com/riwi/eventify/repository/EventRepository.java
@@ -3,9 +3,35 @@ package com.riwi.eventify.repository;
 import com.riwi.eventify.models.Event;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Repository
 public class EventRepository {
-    public void save(Event party) {
+    private List<Event> events = new ArrayList<>();
+
+    public void save(Event event) {
+        events.add(event);
     }
+
+    public List<Event> findAll() {
+        return events;
+    }
+
+    public Event findById(Long id) {
+        //stream() Convierte la lista en flujo para poder filtrar.
+        return events.stream()
+                .filter(e -> e.getId().equals(id))
+                //findFirst() Obtiene el primero encontrado.
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean deleteById(Long id) {
+        return events.removeIf(e -> e.getId().equals(id));
+    }
+
+
+
 }
