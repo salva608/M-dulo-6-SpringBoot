bebd45671d93d8b84ee473dcbab416288a13a196
salva608 <totoalahielmejor@gmail.com>
2026-05-07 21:01:41 -0500

    feat(repository): implement in-memory VenueRepository with Stream API

--- Eventify/src/main/java/com/riwi/eventify/repository/VenueRepository.java
+++ Eventify/src/main/java/com/riwi/eventify/repository/VenueRepository.java
@@ -1,10 +1,33 @@
 package com.riwi.eventify.repository;
 
+import com.riwi.eventify.models.Event;
 import com.riwi.eventify.models.Venue;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class VenueRepository {
-    public void save(Venue place) {
+    private List<Venue> venues = new ArrayList<>();
+
+    public void save(Venue venue) {
+        venues.add(venue);
     }
+
+    public List<Venue> findAll() {
+        return venues;
+    }
+
+    public Venue findById(Long id) {
+        return venues.stream()
+                .filter(v -> v.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean deleteById(Long id) {
+        return venues.removeIf(e -> e.getId().equals(id));
+    }
+
 }
