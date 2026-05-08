97f088289404f64d498e792d1838d313ef86393b
salva608 <totoalahielmejor@gmail.com>
2026-05-07 20:59:35 -0500

    feat(model): create Event entity with Lombok and Swagger documentation

--- Eventify/src/main/java/com/riwi/eventify/models/Event.java
+++ Eventify/src/main/java/com/riwi/eventify/models/Event.java
@@ -1,6 +1,7 @@
 package com.riwi.eventify.models;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,15 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Representa un (Evento) en el ecosistema de Eventify")
 public class Event {
-    public Event(long l, String springBootWorkshop, LocalDate now, String aprenderásDi) {
-    }
+    @Schema(description = "Id unico generado", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
+    private Long id;
+    @Schema(description = "Nombre completo del evento", example = "Concierto de Arcangel")
+    private String name;
+    @Schema(description = "Fecha del evento", example = "15/05/2026")
+    private LocalDate date;
+    @Schema(description = "Descripcion del evento", example = "Increible concierto de Arcangel con invitados especiales")
+    private String description;
+
 }
