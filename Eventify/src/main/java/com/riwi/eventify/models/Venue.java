2af84061a517acddc7c48bf5d10927511f9d7b48
salva608 <totoalahielmejor@gmail.com>
2026-05-07 21:00:39 -0500

    feat(model): create Venue entity with Lombok and Swagger documentation

--- Eventify/src/main/java/com/riwi/eventify/models/Venue.java
+++ Eventify/src/main/java/com/riwi/eventify/models/Venue.java
@@ -1,6 +1,7 @@
 package com.riwi.eventify.models;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,14 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Representa un (lugar) en el ecosistema de Eventify")
 public class Venue {
-    public Venue(long l, String auditorioRiwi, String s, int i) {
-    }
+    @Schema(description = "Id unico generado", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
+    private Long id;
+    @Schema(description = "Nombre completo del lugar", example = "Riwi")
+    private String name;
+    @Schema(description = "Direccion del lugar", example = "Carrera 63a #74-72")
+    private String address;
+    @Schema(description = "Capacidad de personas", example = "100")
+    private int capacity;
 }
