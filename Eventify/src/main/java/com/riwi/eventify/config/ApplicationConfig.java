d77658dfaafa50cc4f664096a71bb7bfddcd158c
salva608 <totoalahielmejor@gmail.com>
2026-05-25 20:39:51 -0500

    update to archivement

--- Eventify/src/main/java/com/riwi/eventify/config/ApplicationConfig.java
+++ Eventify/src/main/java/com/riwi/eventify/config/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package com.riwi.eventify.config;
 
-
 import com.riwi.eventify.models.Event;
 import com.riwi.eventify.models.Venue;
 import com.riwi.eventify.repository.EventRepository;
@@ -10,18 +9,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 @Configuration
 public class ApplicationConfig {
 
     @Bean
     public CommandLineRunner loadData(EventRepository eventRepo, VenueRepository venueRepo) {
         return args -> {
-            // Aquí creas tus objetos iniciales
-            Venue place = new Venue(1L, "Auditorio Riwi", "Calle 123", 100);
-            Event party = new Event(1L, "Spring Boot Workshop", LocalDate.now(), "Aprenderás DI");
+            // Pasamos 'null' en el ID para que Hibernate use el autoincremento (IDENTITY) de forma correcta
+            Venue place = new Venue(null, "Auditorio Riwi", "Calle 123", 100);
+            Event party = new Event(null, "Spring Boot Workshop", LocalDate.now(), "Aprenderás DI");
 
-            // Los guardas en tus repositorios (que ya deben tener el método save)
+            // Guardado seguro en la base de datos en memoria
             venueRepo.save(place);
             eventRepo.save(party);
 
